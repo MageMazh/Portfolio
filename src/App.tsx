4556335fec7d2e86bf7cfe8c2698999e38a3c475
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-12-28 02:57:42 +0800

    add another component feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 
 import "./App.css";
 import Hero from "./component/Hero/Hero";
-import Header from "./component/Header";
+import Header from "./component/Header/Header";
 import About from "./component/About/About";
+import Skill from "./component/Skill/Skill";
+import Project from "./component/Project/Project";
+import Contact from "./component/Contact/Contact";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +14,11 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Skill />
+      <Project />
+      <Contact />
+      <div className="w-full text-center border-t-2 border-slate-700"></div>
+      <Footer />
     </main>
   );
 }
