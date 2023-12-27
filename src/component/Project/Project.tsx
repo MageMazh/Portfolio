ccec4b28f1deb5a9f101c051224848648bf6007a
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-12-28 02:59:18 +0800

    add some code and responsive

--- src/component/Project/Project.tsx
+++ src/component/Project/Project.tsx
@@ -1,47 +1,57 @@
 function Project() {
   const imageProjectnames = [
-    {title: "Grademica", description: "Course grade input management information system"}, 
-    {title: "OKFT", description: 'Website "Organisasi Kemahasiswaan Informatika Fakultas Teknik"'}, 
-    {title: "grademica", description: "My portfolio"}, 
-    {title: "Calculator", description: "A calculator that supports light and dark display modes."}, 
-    {title: "NotesApp", description: "A notes application that implements the Create, Read, Update, and Delete (CRUD) function integrated with the database.", link: "a"}, 
+    {
+      title: "Grademica",
+      description: "Course grade input management information system",
+    },
+    {
+      title: "OKFT",
+      description:
+        'Website "Organisasi Kemahasiswaan Informatika Fakultas Teknik"',
+    },
+    { title: "Portfolio", description: "My personal portfolio" },
+    {
+      title: "Calculator",
+      description: "A calculator that supports light and dark display modes.",
+    },
+    {
+      title: "NotesApp",
+      description:
+        "A notes application that implements the Create, Read, Update, and Delete (CRUD) function integrated with the database.",
+    },
   ];
 
   return (
-    <section id="project" className="px-14 py-14">
-      <div>
+    <main id="project" className="px-14 py-14">
+      <section>
         <h2
-          className="text-xs font-semibold uppercase tracking-widest text-gray-600 font-poppins
-    after:content-[''] after:h-px after:w-11/12 after:bg-primary after:inline-block after:my-1 after:mx-2.5"
+          className="text-xs font-semibold uppercase tracking-widest text-white font-poppins relative 
+      after:content-[''] after:h-px after:w-full after:bg-primary after:inline-block after:my-1 after:mx-auto"
         >
           Projects
         </h2>
-        <div className="text-justify">
-          <p className="relative text-3xl font-bold uppercase font-poppins text-white mb-4">
-            My <span className="text-blue-500">Works</span>
-          </p>
-          <div className="flex flex-wrap gap-6">
-            {imageProjectnames.map((projectname, index) => (
-              <div className="border border-solid border-gray-300 bg-cover bg-center w-[22rem] h-[20rem]">
-                <img
-                  className="object-cover"
-                  src={`/${projectname.title}.png`}
-                  alt={`Image ${index + 1}`}
-                />
-                <div className="flex flex-col gap-4">
-                  <h2 className="px-3 py-2 text-lg font-semibold border-b border-solid border-gray-300">
-                    {projectname.title}
-                  </h2>
-                  <p className="px-3 text-sm">
-                    {projectname.description}
-                  </p>
-                </div>
+        <p className="relative text-3xl font-bold uppercase font-poppins text-white mb-4">
+          My <span className="text-blue-500">Works</span>
+        </p>
+        <section className="flex flex-wrap justify-center xl:justify-start gap-6 text-justify">
+          {imageProjectnames.map((projectname, index) => (
+            <section className="border-2 border-solid border-gray-300 bg-cover bg-center w-[22rem] h-[20rem]">
+              <img
+                className="object-cover border-b"
+                src={`/${projectname.title}.png`}
+                alt={`Image ${index + 1}`}
+              />
+              <div className="flex flex-col gap-4">
+                <h2 className="px-3 py-2 text-lg font-semibold border-b border-solid border-gray-300">
+                  {projectname.title}
+                </h2>
+                <p className="px-3 text-sm">{projectname.description}</p>
               </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
+            </section>
+          ))}
+        </section>
+      </section>
+    </main>
   );
 }
 
