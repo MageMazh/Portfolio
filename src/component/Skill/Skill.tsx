ec53bec936917b41e880aaeb68ef6c212d672c82
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-12-28 02:58:13 +0800

    add responsive

--- src/component/Skill/Skill.tsx
+++ src/component/Skill/Skill.tsx
@@ -16,36 +16,34 @@ function Skill() {
   ];
 
   return (
-    <section id="skill" className="px-14 py-14">
-      <div>
+    <main id="skill" className="px-14 py-14">
+      <section>
         <h2
-          className="text-xs font-semibold uppercase tracking-widest text-gray-600 font-poppins
-      after:content-[''] after:h-px after:w-11/12 after:bg-primary after:inline-block after:my-1 after:mx-2.5"
+          className="text-xs font-semibold uppercase tracking-widest text-white font-poppins relative 
+      after:content-[''] after:h-px after:w-full after:bg-primary after:inline-block after:my-1 after:mx-auto"
         >
           Skills
         </h2>
-        <div className="text-justify">
-          <p className="relative text-3xl font-bold uppercase font-poppins text-white mb-4">
-            My <span className="text-blue-500">Skills</span>
-          </p>
-          <div className="flex flex-wrap gap-4">
-            {imageFilenames.map((filename, index) => (
-              <div
-                key={index}
-                className="flex items-center justify-center w-24 h-24 rounded-2xl border border-solid border-white hover:border-primary hover:shadow-md hover:shadow-primary hover:text-white"
-              >
-                <img
-                  className="w-1/2 h-1/2"
-                  src={`/${filename}.webp`}
-                  alt={`Image ${index + 1}`}
-                  title={`${filename}`}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
+        <p className="relative text-3xl font-bold uppercase font-poppins text-white mb-4">
+          My <span className="text-blue-500">Skills</span>
+        </p>
+        <section className="flex flex-wrap gap-4">
+          {imageFilenames.map((filename, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-center w-24 h-24 rounded-2xl border border-solid border-white hover:border-primary hover:shadow-md hover:shadow-primary hover:text-white"
+            >
+              <img
+                className="w-1/2 h-1/2"
+                src={`/${filename}.webp`}
+                alt={`Image ${index + 1}`}
+                title={`${filename}`}
+              />
+            </div>
+          ))}
+        </section>
+      </section>
+    </main>
   );
 }
 
